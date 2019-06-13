c353704c03c470bea9a8f39165c4db464cb2c105
Manish KC <kcmanish89@gmail.com>
2019-06-12 22:30:40 -0700

    lab_wednesday

--- js/app.js
+++ js/app.js
@@ -15,7 +15,9 @@ function Stores(name, minCustomerPerHour, maxCustomersPerHour, avgCookiePerSale)
 
 /* Generate random numbers of customers based on the flow of maximum and minimum customers per hour */
 Stores.prototype.generateRandomCustomers = function() {
-  var result = Math.floor(Math.random() * this.maxCustomersPerHour) + this.minCustomerPerHour; //Generate random numbers betn: MIN & MAX
+  console.log('max '+ this.maxCustomersPerHour + ' min: '+ this.minCustomerPerHour);
+  var result = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomerPerHour)) + this.minCustomerPerHour; //Generate random numbers betn: MIN & MAX
+  console.log('result: ' +result);
   return result;
 };
 
@@ -34,7 +36,6 @@ Stores.prototype.generateSalesForecast = function() {
   this.sales.push(totalSales); //Make a final push of total cookies
 
 };
-
 /*  Rendering function for the store object */
 Stores.prototype.generateRendering = function() {
   var tableEl = document.getElementById('sales-forecast');
@@ -52,39 +53,26 @@ Stores.prototype.generateRendering = function() {
   }
 
 };
-/**************************************MAIN FUNCTION TO RUN THE SCRIPT*****************************************************************/
-function generateStoreData(a, b, c, d) {
-//Create Objects and store in array of stores
-  var arrayOfStores = [];
-  var totalOfColumns = []; //Stores total of the column
-  // arrayOfStores.push(new Stores('First & Pike', 23, 65, 6.3));
-  // arrayOfStores.push(new Stores('SeaTac Airport', 3, 24, 1.2));
-  // arrayOfStores.push(new Stores('Seattle Center', 11, 38, 3.7));
-  arrayOfStores.push(new Stores('Capitol Hill', 20, 38, 2.3));
-  arrayOfStores.push(new Stores('Alki', 2, 16, 4.6));
 
-  // arrayOfStores.push(new Stores(a, b, c, d));
-  console.log(arrayOfStores); 
+/*****************************************************HELPER FUNCTION*****************************************************************/
 
-  /* Generate Sales Forecast and display elements on table for each stores  */
-  for (var i = 0; i < arrayOfStores.length; i++) {
-    arrayOfStores[i].generateSalesForecast();
-    arrayOfStores[i].generateRendering();
-  }
-
+function generateTotalRows() {
+  ColumnCount++;
+  totalOfColumns = 0;
+  var totalOfColumns = [];
+  var totalColSum;
   /* Generates total of the columns in the display and store in the array*/
-  for (i = 0; i < arrayOfStores[0].sales.length; i++) {
-    var total = 0;
+  for (var i = 0; i < arrayOfStores[0].sales.length; i++) {
+    totalColSum = 0;
     for (var j = 0; j < arrayOfStores.length; j++) {
-      total += arrayOfStores[j].sales[i];
+      totalColSum += arrayOfStores[j].sales[i];
     }
-    console.log(total);
-    totalOfColumns.push(total);
-
+    totalOfColumns.push(totalColSum);
   }
 
   /* Final Rendering for the 'Totals' row */
   var tableEl = document.getElementById('sales-forecast');
+
   var trEl = document.createElement('tr');
   var tdNameEl = document.createElement('td');
   tdNameEl.textContent = 'Totals';
@@ -97,13 +85,31 @@ function generateStoreData(a, b, c, d) {
     trEl.appendChild(tdEl[i]);
     tableEl.appendChild(trEl);
   }
+}
 
-  //test to validate data in console
-  for (i = 0; i < arrayOfStores.length; i++) {
-    console.log(arrayOfStores[i].name);
-    for (j = 0; j < 16; j++) {
-      console.log(arrayOfStores[i].sales[j]);
-    }
+/**************************************MAIN FUNCTION TO RUN THE SCRIPT*****************************************************************/
+var arrayOfStores = [];
+var newRow = -1; //New row when submitted
+var ColumnCount = 0;
+var rowDisplayFlag = false;
+
+function generateStoreData(location, minCustomerPerHour, maxCustomersPerHour, avgCookiePerSale) {
+  rowDisplayFlag = true;
+
+  if (ColumnCount >= 1) {
+    ColumnCount = 0;
+    newRow++;
+    document.getElementById('sales-forecast').deleteRow(-1);
+    arrayOfStores.push(new Stores(location, minCustomerPerHour, maxCustomersPerHour, avgCookiePerSale));
+    arrayOfStores[newRow].generateSalesForecast();
+    arrayOfStores[newRow].generateRendering();
+    generateTotalRows();
+  } else {
+    newRow++;
+    arrayOfStores.push(new Stores(location, minCustomerPerHour, maxCustomersPerHour, avgCookiePerSale));
+    arrayOfStores[newRow].generateSalesForecast();
+    arrayOfStores[newRow].generateRendering();
+    generateTotalRows();
   }
 
 }
@@ -111,19 +117,15 @@ function generateStoreData(a, b, c, d) {
 /* ***********************************DRIVER**********************************************************************************************************/
 
 var form = document.getElementById('add-store');
-
 var handleFormSubmit = function(formSubmitEvent){
+
   formSubmitEvent.preventDefault();
-  
   var locationName = formSubmitEvent.target.locationName.value;
   var minCustomer = formSubmitEvent.target.minCustomer.value;
   var maxCustomer = formSubmitEvent.target.maxCustomer.value;
   var avgCookies = formSubmitEvent.target.avgCookies.value;
   
-  
-  console.log(locationName, minCustomer, maxCustomer, avgCookies);
-  generateStoreData(locationName, minCustomer, maxCustomer, avgCookies);
-
+  generateStoreData(locationName, parseInt(minCustomer), parseInt(maxCustomer), parseFloat(avgCookies));
 };
-
 form.addEventListener('submit', handleFormSubmit);
+
